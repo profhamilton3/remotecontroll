01dbf9ed1f7cad795cc09c0881e7f6dd7e21e7c5
T. Hamilton <profhamilton3@gmail.com>
2022-03-25 17:39:06 -0400

    update

--- main.ts
+++ main.ts
@@ -1,14 +1,16 @@
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
     radio.sendString("DN")
     basic.showArrow(ArrowNames.SouthEast)
+    basic.pause(200)
 })
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
     basic.showString("A")
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
-    radio.sendValue("speed", -10)
+    radio.sendValue("speed", -50)
     basic.showArrow(ArrowNames.South)
+    basic.pause(200)
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(0)
@@ -17,10 +19,12 @@ input.onButtonPressed(Button.B, function () {
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
     radio.sendString("UP")
     basic.showArrow(ArrowNames.NorthEast)
+    basic.pause(200)
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
-    radio.sendValue("speed", 10)
+    radio.sendValue("speed", 50)
     basic.showArrow(ArrowNames.North)
+    basic.pause(200)
 })
 joystickbit.initJoystickBit()
 radio.setGroup(1)
@@ -31,16 +35,14 @@ let pY0 = joystickbit.getRockerValue(joystickbit.rockerType.Y)
 let pX0 = joystickbit.getRockerValue(joystickbit.rockerType.X)
 let pX = pX0
 let pY = pY0
-basic.showIcon(IconNames.Yes)
+basic.showIcon(IconNames.SmallSquare)
 basic.forever(function () {
     if (Math.abs(joystickbit.getRockerValue(joystickbit.rockerType.X) - pX) > 50 && Math.abs(joystickbit.getRockerValue(joystickbit.rockerType.X) - pX0) > 50) {
         radio.sendValue("vx", joystickbit.getRockerValue(joystickbit.rockerType.X))
-        basic.pause(200)
-        pX0 = joystickbit.getRockerValue(joystickbit.rockerType.X)
+        pX = joystickbit.getRockerValue(joystickbit.rockerType.X)
     }
     if (Math.abs(joystickbit.getRockerValue(joystickbit.rockerType.Y) - pY) > 50 && Math.abs(joystickbit.getRockerValue(joystickbit.rockerType.Y) - pY0) > 50) {
         radio.sendValue("vy", joystickbit.getRockerValue(joystickbit.rockerType.Y))
-        basic.pause(200)
         pY = joystickbit.getRockerValue(joystickbit.rockerType.Y)
     }
     basic.pause(200)
